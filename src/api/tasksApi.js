2d8a9fdd99d06033f947b483f16dba7162f045ce
ELVIG17 <elvig.shakirov@gmail.com>
2026-03-14 11:01:32 +0000

    front-end communication with the backend

---
+++ src/api/tasksApi.js
@@ -0,0 +1,48 @@
+async function request(path, options){
+      const res = await fetch(path, {
+    headers: { "Content-Type": "application/json" },
+    ...options,
+  });
+
+
+  if (!res.ok){
+    const message = await res.text()
+    throw new Error(message || `Request failed ${res.status}`)
+  }
+
+  if(res.status === 204) return null
+
+  return res.json()
+
+   
+}
+
+ export const tasksApi = {
+    
+        list (){
+        return request("/api/tasks")
+    },
+
+    create(data){
+        return request(`/api/tasks`, {
+            method: "POST", 
+            body: JSON.stringify(data)
+        })
+    }, 
+
+    update(id, data){
+        return request(`/api/tasks/${id}`, {
+            method: "PATCH", 
+            body:JSON.stringify(data)
+        })
+    }, 
+
+    remove(id) {
+        return request(`/api/tasks/${id}`, {
+            method: "DELETE", 
+        })
+    }
+
+
+
+}
